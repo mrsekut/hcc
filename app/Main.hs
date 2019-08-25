57fca29d136fd01d1d28c9463b18de73f7ff40dc
mrsekut <k.cloudspider@gmail.com>
2019-08-25 20:21:47 +0900

    [Add] use class

--- app/Main.hs
+++ app/Main.hs
@@ -10,92 +10,104 @@ import           Text.Parsec
 asmHeader = mapM_ putStrLn [".intel_syntax noprefix", ".global hcc", "hcc:"]
 
 
+class Reifiable a where
+    reify :: a -> [ String ]
+
+instance Reifiable Expr where
+    reify (Nat i) = ["    push " ++ show i]
+    reify (Add e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    add rax, rdi"
+               , "    push rax"
+               ]
+    reify (Sub e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    sub rax, rdi"
+               , "    push rax"
+               ]
+    reify (Mul e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ ["    pop rdi", "    pop rax", "    imul rdi", "    push rax"]
+    reify (Div e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cqo"
+               , "    idiv rdi"
+               , "    push rax"
+               ]
+    reify (Eq e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    sete al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+    reify (Neq e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    setne al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+    reify (Lt e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    setl al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+    reify (Lte e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    setle al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+    reify (Gt e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    setg al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+    reify (Gte e1 e2) =
+        reify e1
+            ++ reify e2
+            ++ [ "    pop rdi"
+               , "    pop rax"
+               , "    cmp rax, rdi"
+               , "    setge al"
+               , "    movzb rax, al"
+               , "    push rax"
+               ]
+
+
 gen :: Expr -> IO ()
-gen e = case e of
-    Nat i     -> putStrLn $ "    push " ++ show i
-    Add e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    add rax, rdi"
-        putStrLn "    push rax"
-    Sub e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    sub rax, rdi"
-        putStrLn "    push rax"
-    Mul e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    imul rdi"
-        putStrLn "    push rax"
-    Div e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cqo"
-        putStrLn "    idiv rdi"
-        putStrLn "    push rax"
-    Eq e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    sete al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
-    Neq e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    setne al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
-    Lt e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    setl al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
-    Lte e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    setle al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
-    Gt e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    setg al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
-    Gte e1 e2 -> do
-        gen e1
-        gen e2
-        putStrLn "    pop rdi"
-        putStrLn "    pop rax"
-        putStrLn "    cmp rax, rdi"
-        putStrLn "    setge al"
-        putStrLn "    movzb rax, al"
-        putStrLn "    push rax"
+gen = mapM_ putStrLn . reify
 
 
 main :: IO ()
--- tmp.s
+++ tmp.s
@@ -2,12 +2,15 @@
 .global hcc
 hcc:
     push 12
-    push 5
+    push 3
     pop rdi
     pop rax
-    cmp rax, rdi
-    setl al
-    movzb rax, al
+    imul rdi
+    push rax
+    push 9
+    pop rdi
+    pop rax
+    sub rax, rdi
     push rax
     pop rax
     ret
