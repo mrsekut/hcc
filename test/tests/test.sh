994f826941937d7de6f521ed59860e5a4aa8f5a6
mrsekut <k.cloudspider@gmail.com>
2019-09-16 18:15:20 +0900

    [Add]host testcode

---
+++ test/tests/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while read row; do
+    index=$(echo ${row} | cut -d , -f1)
+    input=$(echo ${row} | cut -d , -f2)
+
+    touch ./tmps/$index.s
+    stack exec hcc-exe "$input" >./tmps/$index.s
+done <tests.csv
--- tmp.s
+++ tmp.s
@@ -4,7 +4,43 @@ hcc:
     push rbp
     mov rbp, rsp
     sub rsp, 208
+    mov rax, rbp
+    sub rax, 64
+    push rax
     push 3
+    pop rdi
+    pop rax
+    mov [rax], rdi
+    push rdi
+    mov rax, rbp
+    sub rax, 128
+    push rax
+    push 5
+    push 10
+    pop rdi
+    pop rax
+    add rax, rdi
+    push rax
+    pop rdi
+    pop rax
+    mov [rax], rdi
+    push rdi
+    mov rax, rbp
+    sub rax, 128
+    push rax
+    pop rax
+    mov rax, [rax]
+    push rax
+    mov rax, rbp
+    sub rax, 64
+    push rax
+    pop rax
+    mov rax, [rax]
+    push rax
+    pop rdi
+    pop rax
+    add rax, rdi
+    push rax
     pop rax
     mov rsp, rbp
     pop rbp
